8065a21caa158b2f6134a38099b7d876838e4838
r-o-h-a-n-2004 <134271385+r-o-h-a-n-2004@users.noreply.github.com>
2025-10-13 14:52:38 +0530

    Time: 2 ms (74.70%) | Memory: 14.9 MB (36.04%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,25 +1,17 @@
 #include <vector>
-
 using namespace std;
-
 class Solution {
 public:
-    // The logic is inside a method called "twoSum"
-    // It accepts the inputs as parameters: vector<int>& nums, int target
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        // The core logic for finding the pair remains the same
+        int difference=0;
+        unordered_map<int,int> m1;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                
-                if (nums[i] + nums[j] == target) {
-                    // Instead of printing, we return the result
-                    return {i, j};
-                }
+            difference=target-nums[i];
+            if(m1.find(difference)!=m1.end()){
+                return{m1[difference] , i};
             }
+            m1[nums[i]]=i;
         }
-        
-        // Return an empty vector if no solution is found
         return {};
     }
 };
