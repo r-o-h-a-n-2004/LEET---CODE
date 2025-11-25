993ebd97055ae1c81254bd389940d71e0509c144
r-o-h-a-n-2004 <134271385+r-o-h-a-n-2004@users.noreply.github.com>
2025-11-25 21:57:04 +0530

    Time: 5 ms (1.26%) | Memory: 15.7 MB (32.09%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int brek=-1;
+        int j=-1;
+        for(int i=n-1; i>0; i--){
+            if(nums[i-1]<nums[i]){
+                brek=nums[i-1];
+                j=i-1;
+                break;
+            }
+        }
+
+        if(j!=-1){
+            for(int i=n-1; i>0; i--){
+                if(nums[i]>brek){
+                    swap(nums[i],nums[j]);
+                    break;
+                }
+            }
+            reverse(nums.begin() + j+1, nums.begin() + (n-1) + 1);
+
+            for(int i=0;i<n;i++){
+                cout<<nums[i];
+            }
+        }
+        else{
+            reverse(nums.begin() + 0, nums.begin() + (n-1) + 1);
+            for(int i=0;i<n;i++){
+                cout<<nums[i];
+            }
+        }
+    }
+};
